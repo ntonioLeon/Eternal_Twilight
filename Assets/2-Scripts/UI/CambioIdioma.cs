1bd60d7d776bf65df172d897a705ab0bd9513c49
ntonioLeon <126107644+ntonioLeon@users.noreply.github.com>
2024-04-08 17:21:51 +0200

    Localization
    Cambia de idiomas, escenas pendientes.

---
+++ Assets/2-Scripts/UI/CambioIdioma.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+
+public class CambioIdioma : MonoBehaviour
+{
+    public string[] idioma = {"English", "Español"};
+    public Text idiomaText;
+    private int indiceIdioma;
+
+    public void CambiarIdioma()
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[indiceIdioma];
+    }
+
+    public void CambiarDerecha() 
+    {
+        
+        if (!idiomaText.text.Equals(idioma[idioma.Length-1]))
+        {
+            indiceIdioma += 1;
+            idiomaText.text = idioma[indiceIdioma];            
+            CambiarIdioma();
+        } 
+        else
+        {
+            indiceIdioma = 0;
+            idiomaText.text = idioma[indiceIdioma];
+            CambiarIdioma();
+        }
+    }
+
+    public void CambiarIzquierda()
+    {
+        if (!idiomaText.text.Equals(idioma[0]))
+        {
+            indiceIdioma -= 1;
+            idiomaText.text = idioma[indiceIdioma];
+            CambiarIdioma();
+        }
+        else
+        {
+            indiceIdioma = idioma.Length-1;
+            idiomaText.text = idioma[indiceIdioma];
+            CambiarIdioma();
+        }
+    }
+
+}
