180056e74b63cb3105f8e86ade6a80b73a118766
ntonioLeon <126107644+ntonioLeon@users.noreply.github.com>
2024-05-23 13:32:45 +0200

    Todo mal

---
+++ Assets/2-Scripts/Escena/FinDemo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class FinDemo : MonoBehaviour
+{    
+    [SerializeField] private PlayableDirector playableDirector;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {            
+            playableDirector.Play();
+            GetComponent<BoxCollider2D>().enabled = false;
+            //Aqui se lanza la pantalla de puntos
+        }
+    }
+}
