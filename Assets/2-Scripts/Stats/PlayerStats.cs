61e7a1a55c53e03baadfc01a52ffd73bea27d231
Jorge Pastor Rodriguez <116525569+yuripastorovic@users.noreply.github.com>
2024-06-05 19:41:42 +0200

    StatsUI

--- Assets/2-Scripts/Stats/PlayerStats.cs
+++ Assets/2-Scripts/Stats/PlayerStats.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : CharacterStats
 {
+    [SerializeField] private Text maxHealthT;
+    [SerializeField] private Text magicResistT;
+    [SerializeField] private Text fireDMGT;
+    [SerializeField] private Text iceDMGT;
+    [SerializeField] private Text lightDMGT;
+    [SerializeField] private Text critPOT;
+    [SerializeField] private Text critCT;
+    [SerializeField] private Text damageT;
+    [SerializeField] private Text armorT;
+    [SerializeField] private Text evasionT;
+
     private Player player;
     protected override void Start()
     {
         base.Start();
 
         player = GetComponent<Player>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        RefreshStats();
     }
 
     public override void TakeDamage(float dmg)
@@ -24,5 +42,19 @@ public class PlayerStats : CharacterStats
         AudioManager.instance.PlayBattle();
         player.Die();
         GetComponent<PlayerItemDrop>().GenerateDrop();
+    }
+
+    public void RefreshStats()
+    {
+        maxHealthT.text = maxHealth.GetValue().ToString();
+        magicResistT.text = magicResistance.GetValue().ToString();
+        fireDMGT.text = fireDamage.GetValue().ToString();
+        iceDMGT.text = iceDamage.GetValue().ToString();
+        lightDMGT.text = lightningDamage.GetValue().ToString();
+        critPOT.text = critPower.GetValue().ToString();
+        critCT.text = critChance.GetValue().ToString();
+        damageT.text = damage.GetValue().ToString();
+        armorT.text = armor.GetValue().ToString();
+        evasionT.text = evasion.GetValue().ToString();
     }
 }
